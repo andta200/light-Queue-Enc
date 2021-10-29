6f8dec54672f74002ba43b3fc7ac9d99264e4949
Col-Serra <79519115+Col-Serra@users.noreply.github.com>
2021-10-30 00:04:55 +0100

    Update worker.py

--- bot/worker.py
+++ bot/worker.py
@@ -189,7 +189,7 @@ async def encod(event):
                     event.media,
                     dir,
                     progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                        progress(d, t, xxx, ttt, "`Downloading…`)
+                        progress(d, t, xxx, ttt, "`Downloading`)
                     ),
                 )
         except Exception as er:
